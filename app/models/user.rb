21e50879be8298af51f0fd7cf7eea3495d950297
bo cimino <bothemasterofall@gmail.com>
2014-10-10 18:13:54 -0500

    added encrypted passwords and salt to the users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -25,4 +27,38 @@ class User < ActiveRecord::Base
 	validates :password, :presence => true,
 		:confirmation => true,	#creates the password_confirmation attribute
 		:length => {:minimum => 6}
+
+	#executes the methods before the user is saved
+	before_save :encrypt_password 
+
+	def has_password?(pass)
+		self.encrypted_password == encrypt(pass)
+	end
+
+	class << self
+		def authenticate(email, sub_pass)	
+			user = find_by_email(email)
+			return nil if user.nil?
+			return user if user.has_password?(sub_pass)
+		end
+	end
+
+	private
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
+end
 end

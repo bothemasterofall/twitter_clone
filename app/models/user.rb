ce5fba4838c30751e16ad5b38d64c00a3fb51a0f
bo cimino <bothemasterofall@gmail.com>
2014-10-10 18:30:40 -0500

    added fields to signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,6 @@
 class User < ActiveRecord::Base
 	attr_accessor :password
 
-	#everything the web needs to access needs to be on this line
-	attr_accessible :name, :email, :password, :password_confirmation
-
 	email_regex = /\A[a-z\.\d]+@[a-z\.\d]+\.[a-z]+\z/i
 
 	validates :name, :presence => true,

6304b586c4e62077c6ac7a0db40a5eb401e13b9d
bo cimino <bothemasterofall@gmail.com>
2014-10-10 13:21:47 -0500

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
+  root 'pages#home'
   get 'pages/contact'
-
   get 'pages/about'
-
   get 'pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.

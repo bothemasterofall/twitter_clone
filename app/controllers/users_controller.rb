36982e07d0a98ee503379d81e2e659b2f928460e
bo cimino <bothemasterofall@gmail.com>
2014-10-11 14:41:19 -0500

    made it possible to sign in and see the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
   def show
-  	@user = User.find(params[:id])
-  	@title = @user.name
+	  	@user = User.find(params[:id])
+	  	@title = @user.name
   end
 
   def new
-  	@user = User.new
-  	@title = "Sign up"
+	  	@user = User.new
+	  	@title = "Sign up"
   end
+
+  	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user, :flash => {:success => "Welcome to my Twitter Clone"}
+		else
+			@title = "Sign up"
+			render 'new'
+		end	
+  	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end

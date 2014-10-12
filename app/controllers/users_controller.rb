a24f762b49fe8d08359cb26c88716275da71c06a
bo cimino <bothemasterofall@gmail.com>
2014-10-12 04:50:35 -0500

    can now edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
-  def show
-	  	@user = User.find_by_id(params[:id])
-	  	@title = @user.name
-  end
-
-  def new
-	  	@user = User.new
-	  	@title = "Sign up"
-  end
+	def index
+  		@users = User.paginate(:page => params[:page])
+  		@title = "All users"
+  	end
 
   	def create
 		@user = User.new(user_params)
@@ -19,6 +14,31 @@ class UsersController < ApplicationController
 			render 'new'
 		end	
   	end
+
+  	def new
+	  	@user = User.new
+	  	@title = "Sign up"
+	end
+
+  	def edit
+  		@user = User.find(params[:id])
+  		@title = "Edit user"
+  	end
+
+  	def show
+	  	@user = User.find_by_id(params[:id])
+	  	@title = @user.name
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			redirect_to @user, :flash => {:success => "Info updated!"}
+		else
+			@title = "Edit user"
+			render 'edit'
+		end
+	end
 
 	private
 		def user_params

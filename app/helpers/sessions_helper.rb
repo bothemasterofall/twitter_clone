9a46e237f7b1108051ef3225720226254a4ddf02
bo cimino <bothemasterofall@gmail.com>
2014-10-12 05:21:02 -0500

    added index to users controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,6 +38,11 @@ module SessionsHelper
 
 	def redirect_back_or(default)
 		redirect_to(session[:return_to] || default)
+		clear_return_to
+	end
+
+	def clear_return_to
+		session[:return_to] = nil
 	end
 
 	private

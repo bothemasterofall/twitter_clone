485299c946c68eb5d56dfd61a81999ce02f9d4b1
bo cimino <bothemasterofall@gmail.com>
2014-10-11 21:54:11 -0500

    you can now sign in. Made sessions_helper, and told sessions create method to redirect to user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.authenticate(params[:session][:email],
+			params[:session][:password])
+		if user.nil?
+			flash.now[:error] = "Invalid email/password combination" #.now persists only for this request
+			render 'new'
+		else
+			sign_in user
+			redirect_to user
+		end
+	end
+
+	def destroy
 	end
 end

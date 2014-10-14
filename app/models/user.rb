2d2a4a79f40abd19265094706f2c3ffdf148ead1
bo cimino <bothemasterofall@gmail.com>
2014-10-13 22:45:31 -0500

    added micropost model with user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
 	#everything the web needs to access needs to be on this line
 	attr_accessible :name, :email, :password, :password_confirmation
 
+	has_many :microposts
+
 	email_regex = /\A[a-z\.\d]+@[a-z\.\d]+\.[a-z]+\z/i
 
 	validates :name, :presence => true,

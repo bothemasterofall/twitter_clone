edce34d5416082722c15081583313bdc0d37c29d
bo cimino <bothemasterofall@gmail.com>
2014-10-20 16:19:39 -0500

    removed the default scope line

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -16,6 +16,4 @@ class Micropost < ActiveRecord::Base
 
 	validates :content, :presence => true, :length => { :maximum => 140}
 	validates :user_id, :presence => true
-
-	default_scope :order => 'created_at DESC'
 end

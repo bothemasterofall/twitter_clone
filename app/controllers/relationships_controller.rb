c3a3c6a808127ae20f5820a404356135a5d3b4b4
bo cimino <bothemasterofall@gmail.com>
2014-10-22 00:55:03 -0500

    added sample data generator

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,18 @@ class RelationshipsController < ApplicationController
 	def create
 		@user = User.find(params[:relationship][:followed_id])
 		current_user.follow!(@user)
-		redirect_to @user 
+		respond_to do |format|
+			format.html { redirect_to @user }
+			format.js #rails will by default laucnch create.js.erb
+		end
 	end
 
 	def destroy
 		@user = Relationship.find(params[:id]).followed
 		current_user.unfollow!(@user)
-		redirect_to @user
+		respond_to do |format|
+			format.html { redirect_to @user }
+			format.js #rails will by default laucnch create.js.erb
+		end
 	end
 end
